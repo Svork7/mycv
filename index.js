c4ddc9dbbca2b1ffa492a12f9057bf7a9da35de0
Eugene Mikhailov <svork@ya.ru>
2022-12-05 21:03:36 +0300

    added burger button

--- index.js
+++ index.js
@@ -347,6 +347,13 @@ for (let buttonIndex = 0; buttonIndex < buttons.length; buttonIndex++) {
   button.liquidButton = new LiquidButton(button)
 }
 
-$('.menu__btn').on('click', function () {
+/*$('.menu__btn').on('click', function () {
   $('.menu__list').toggleClass('menu__list--active')
+})*/
+
+$('.menu__btn').addClass('unToggled')
+$('.menu__btn').click(function () {
+  $(this).toggleClass('toggled')
+  $(this).toggleClass('unToggled')
+  // also open menu here...
 })
